5e5e425b57fea1ad747c726c0232f3a2573e9ddd
suneelt68 <30464384+suneelt68@users.noreply.github.com>
2025-08-10 14:45:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,6 @@ from fastapi import FastAPI
 from nsepythonserver import nse_quote_ltp
 
 app = FastAPI()
-data = {
-        "23000": safe_nse_quote_ltp("NIFTY", "30-Sep-2025", "PE", 23000),
-        "25800": safe_nse_quote_ltp("NIFTY", "30-Sep-2025", "CE", 25800)
-    }
-
-print(data)
 
 def safe_nse_quote_ltp(symbol, expiry, option_type, strike):
     try:
@@ -19,12 +13,14 @@ def safe_nse_quote_ltp(symbol, expiry, option_type, strike):
         # Catch-all for any other errors
         return None
 
+
 @app.get("/nse/quote_ltp")
 def get_quote_ltp():
     data = {
         "23000": safe_nse_quote_ltp("NIFTY", "30-Sep-2025", "PE", 23000),
         "25800": safe_nse_quote_ltp("NIFTY", "30-Sep-2025", "CE", 25800)
     }
+    print(data)
     # Optionally filter out None values if you want
     filtered_data = {k: v for k, v in data.items() if v is not None}
     return filtered_data
